ff514acfee2515b3525dac6389916dc5e5bc77ae
Ajay Nagesh <ajaynagesh@Ajays-MacBook-Pro.local>
2018-05-10 01:59:45 -0700

    saving the run_name for ease of post-processing - small change

--- pytorch/mean_teacher/run_context.py
+++ pytorch/mean_teacher/run_context.py
@@ -47,7 +47,7 @@ class RunContext:
 
     def __init__(self, runner_file, run_idx, run_name):
         logging.basicConfig(level=logging.INFO, format='%(message)s')
-        runner_name = os.path.basename(runner_file).split(".")[0] + run_name
+        runner_name = os.path.basename(runner_file).split(".")[0] + '_' + run_name
         self.result_dir = "{root}/{runner_name}/{date:%Y-%m-%d_%H:%M:%S}/{run_idx}".format(
             root='results',
             runner_name=runner_name,
