fix simple_MLP_embed_RE model, apply mask so that padded items does not effect backprop

pytorch/main.py
@@ -99,7 +99,6 @@ def main(context):
         if args.dataset in ['conll', 'ontonotes', 'riedel', 'gids']:
             model_params['word_vocab_embed'] = word_vocab_embed
             model_params['word_vocab_size'] = word_vocab_size
-            # model_params['padding_idx'] = padding_idx
             model_params['wordemb_size'] = args.wordemb_size
             model_params['hidden_size'] = args.hidden_size
             model_params['update_pretrained_wordemb'] = args.update_pretrained_wordemb
@@ -533,17 +532,20 @@ def train(train_loader, model, ema_model, optimizer, epoch, dataset, log):
 
         # US - TRAIN!!
         elif args.dataset in ['riedel', 'gids'] and args.arch == 'lstm_RE':
-            model_out, perm_idx = model((input_var, seq_lengths))
+            model_out, perm_idx = model((input_var, seq_lengths))            # model_out: size of one batch(256) * score of each label (torch.FloatTensor of size 56)
             ema_model_out, perm_idx_ema = ema_model((ema_input_var, seq_lengths))
-            if (perm_idx_ema.ne(perm_idx).sum() > 0):
-                print('perm_idx_ema != perm_idx')
+            assert perm_idx_ema.ne(perm_idx).sum() == 0
 
             target_var = target_var[perm_idx]
 
-        # elif args.dataset in ['riedel', 'gids'] and args.arch == 'simple_MLP_embed_RE':
-        #     ema_model_out = ema_model(ema_entity1_var, ema_entity2_var, ema_inbetween_chunk_var)
-        #     model_out = model(entity1_var, entity2_var, inbetween_chunk_var)
-        #     # model_out: size of one batch(256) * score of each label (torch.FloatTensor of size 56)
+        elif args.dataset in ['riedel', 'gids'] and args.arch == 'simple_MLP_embed_RE':
+
+            model_out, perm_idx = model((input_var, seq_lengths, dataset.pad_id))
+            ema_model_out, perm_idx_ema = ema_model((ema_input_var, seq_lengths, dataset.pad_id))
+            assert perm_idx_ema.ne(perm_idx).sum() == 0
+
+            target_var = target_var[perm_idx]
+
         else:
             ema_model_out = ema_model(ema_input_var)
             model_out = model(input_var)
@@ -854,9 +856,11 @@ def validate(eval_loader, model, log, global_step, epoch, dataset, result_dir, m
             output1, perm_idx_test = model((input_var, seq_lengths))
             target_var = target_var[perm_idx_test]
 
-        # ## AVG MODEL
-        # elif args.dataset in ['riedel', 'gids'] and args.arch == 'simple_MLP_embed_RE':
-        #     output1 = model(entity1_var, entity2_var, inbetween_chunk_var)
+        ## AVG MODEL
+        elif args.dataset in ['riedel', 'gids'] and args.arch == 'simple_MLP_embed_RE':
+            output1, perm_idx_test = model((input_var, seq_lengths, dataset.pad_id))
+            target_var = target_var[perm_idx_test]
+
         else:
             output1 = model(input_var) ##, output2 = model(input_var)
         #softmax1, softmax2 = F.softmax(output1, dim=1), F.softmax(output2, dim=1)

pytorch/mean_teacher/architectures.py
@@ -182,7 +182,7 @@ class FeedForwardMLPEmbed(nn.Module):
 ##### Simple architecture for relation extraction. the entity and sentence embeddings are computed by an average
 ##############################################
 @export
-def simple_MLP_embed_RE(word_vocab_size, num_classes, pretrained=True, word_vocab_embed=None, wordemb_size=300, hidden_size=200, update_pretrained_wordemb=False):
+def simple_MLP_embed_RE(word_vocab_size, num_classes, wordemb_size, pretrained=True, word_vocab_embed=None, hidden_size=200, update_pretrained_wordemb=False):
 
     model = FeedForwardMLPEmbed_RE(word_vocab_size, wordemb_size, hidden_size, num_classes, word_vocab_embed, update_pretrained_wordemb)
     return model
@@ -215,14 +215,32 @@ class FeedForwardMLPEmbed_RE(nn.Module):
         self.layer2 = nn.Linear(hidden_sz, output_sz, bias=True)
         # self.softmax = nn.Softmax(dim=1) ## IMPT NOTE: Removing the softmax from here as it is done in the loss function
 
-    def forward(self, input_tuple):
+    def forward(self, input_tuple, pad_id):
         input = input_tuple[0]
         seq_lengths = input_tuple[1]
 
-        # todo: need to add mask so that padding does not effect backprop
-        embed = torch.mean(self.embeddings(input), 1)  # Note: Average the word-embeddings
+        # Embed the input
+        embedded = self.embeddings(input)
+        print('embedded.shape: ' + str(embedded.shape))
 
-        res = self.layer1(embed)
+        # Make the mask for removing the padded items
+        mask = input.ne(pad_id)
+        # mask = mask.type(torch.LongTensor)
+        mask = mask.type(torch.FloatTensor)
+
+        # add an extra dimension, initially of size 1
+        # then "expand_as" copies the last dimension into the new dimension
+        # This essentially propogates the mask through the final dimension
+        # input.shape[0] should be batch size and input.shape[1] should be the num_words
+        expanded_mask = mask.view(input.shape[0], input.shape[1], 1).expand_as(embedded)
+
+        # Apply mask (clear out the embeddings of padded items)
+        masked_embedded = embedded * expanded_mask
+
+        summation = masked_embedded.sum(1)
+        avg = summation / seq_lengths.view(-1,1).expand_as(summation)
+
+        res = self.layer1(avg)
         res = self.activation(res)
         res = self.layer2(res)
 
@@ -232,7 +250,7 @@ class FeedForwardMLPEmbed_RE(nn.Module):
 ##### More advanced architecture where the entity and words in-between embeddings are computed by a Sequence model (like a biLSTM) and then concatenated together
 ##############################################
 @export
-def lstm_RE(word_vocab_size, num_classes, hidden_size, pretrained=True, wordemb_size=300, word_vocab_embed=None, update_pretrained_wordemb=False):
+def lstm_RE(word_vocab_size, num_classes, wordemb_size, hidden_size, pretrained=True, word_vocab_embed=None, update_pretrained_wordemb=False):
     lstm_hidden_size = 50 # was 100
     model = SeqModel_RE(word_vocab_size, wordemb_size, lstm_hidden_size, hidden_size, num_classes, word_vocab_embed, update_pretrained_wordemb)
     return model